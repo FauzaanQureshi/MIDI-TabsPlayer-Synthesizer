ee0582811d8d3615d860b0fe2e4c601fc9b9706f
Fauzaan Qureshi <42518372+FauzaanQureshi@users.noreply.github.com>
2020-07-25 16:30:40 +0530

    Update MusicSheetReader.java

--- JavaProg/MusicSheetReader.java
+++ JavaProg/MusicSheetReader.java
@@ -1,30 +1,63 @@
 package JavaProg;
 
 import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Arrays;
+
 //import java.util.Scanner;
 
 import JavaProg.SimpleSynth;
 
 public class MusicSheetReader{
-	static int counter = 0;
-	static int nano = 0;
+	//static int counter = 0;
+	//static int nano = 0;
 	public static void main(String[] args) throws InterruptedException{
 		SimpleSynth synth = new SimpleSynth();
 		
+		/* Set of functional keys */
+		HashSet<Character> keys = new HashSet<>();
+		keys.add('-');
+		keys.add('_');
+		keys.add('.');
+		keys.add('A');
+		keys.add('B');
+		keys.add('C');
+		keys.add('D');
+		keys.add('E');
+		keys.add('F');
+		keys.add('G');
+		keys.add('b');
+		keys.add('s');
+		keys.add('i');
+		keys.add('m');
+		keys.add('q');
+		
 		char key,octave;
 		key = octave = 0;
-		
-		//int counter = 0;
+		int i = 0;
 		
 		FileReader sheet = null;
+		String f_arr[] = new File("C:\\Users\\Fauzaan\\Desktop\\MusicSheets\\").list((d, s) -> {return s.toLowerCase().endsWith(".sheet");});
+		for(String f : f_arr){System.out.println(String.format("%d %s",i++,f));}
+		System.out.println("\n\nEnter File Index: ");
+		//System.exit(0);
 		try {
-			sheet = new FileReader("Sheet_1.txt");
+			String f = f_arr[new Scanner(System.in).nextInt()];
+			System.out.println("C:\\Users\\Fauzaan\\Desktop\\MusicSheets\\"+f);
+			sheet = new FileReader("C:\\Users\\Fauzaan\\Desktop\\MusicSheets\\"+f);
 		} catch (FileNotFoundException e2) {e2.printStackTrace();}
-		char note[] = {0,0,0}; 
+		
+		int instrument = 0;
+		char note[] = {0,0,0};
+		char ins[]={' ',' ',' '}; 
+		boolean flag = false;
+		
 		Map<String, Integer> kMap = new HashMap<String,Integer>();
 		
 		kMap.put("C0 ", 12);
@@ -66,31 +99,50 @@ public class MusicSheetReader{
 		kMap.put("A#3", 58);
 		kMap.put("B3 ", 59);
 		kMap.put("C4 ", 60);
+		kMap.put("C#4", 61);
 		kMap.put("D4 ", 62);
+		kMap.put("D#4", 63);
 		kMap.put("E4 ", 64);
 		kMap.put("F4 ", 65);
+		kMap.put("F#4", 66);
 		kMap.put("G4 ", 67);
+		kMap.put("G#4", 68);
 		kMap.put("A4 ", 69);
+		kMap.put("A#4", 70);
 		kMap.put("B4 ", 71);
 		kMap.put("C5 ", 72);
+		kMap.put("C#5", 73);
 		kMap.put("D5 ", 74);
+		kMap.put("D#5", 75);
 		kMap.put("E5 ", 76);
 		kMap.put("F5 ", 77);
+		kMap.put("F#5", 78);
 		kMap.put("G5 ", 79);
+		kMap.put("G#5", 80);
 		kMap.put("A5 ", 81);
+		kMap.put("A#5", 82);
 		kMap.put("B5 ", 83);
 		kMap.put("C6 ", 84);
+		kMap.put("C#6", 85);
 		kMap.put("D6 ", 86);
+		kMap.put("D#6", 87);
 		kMap.put("E6 ", 88);
 		kMap.put("F6 ", 89);
+		kMap.put("F#6", 90);
 		kMap.put("G6 ", 91);
+		kMap.put("G#6", 92);
 		kMap.put("A6 ", 93);
+		kMap.put("A#6", 94);
 		kMap.put("B6 ", 95);
 		kMap.put("C7 ", 96);
+		kMap.put("C#7", 97);
 		kMap.put("D7 ", 98);
+		kMap.put("D#7", 99);
 		kMap.put("E7 ",100);
 		kMap.put("F7 ",101);
+		kMap.put("F#7", 102);
 		kMap.put("G7 ",103);
+		kMap.put("G#7", 104);
 		kMap.put("A7 ",105);
 		kMap.put("B7 ",107);
 		
@@ -98,30 +150,76 @@ public class MusicSheetReader{
 			try {
 
 				key = (char) sheet.read();
-				if (key !='-' && key !='.' && key !='q' && key !='A'&& key !='B'&& key !='b'&& key !='C'&& key !='D'&& key !='E'&& key !='F'&& key !='G')
+				if (key=='<'){  /*Comments in File*/
+					key = (char) sheet.read();
+					while (key!='>'){
+						System.out.print(key);
+						key = (char) sheet.read();
+					}
+					System.out.println(' ');
+				}
+				
+				if (!keys.contains(key)){ /*If not func key, read next*/
 					key=0;
+				}
+				
+				if (key=='i'){ /*Change Instrument for single note*/
+					flag = true;
+					sheet.read();
+					key = (char) sheet.read();
+					Arrays.fill(ins, ' ');
+					i = 2;
+					
+					while (key!='>'){
+						ins[i--] = key;
+						key = (char) sheet.read();
+					}
+					key = ins[2];
+					ins[2] = ins[0];
+					ins[0] = key;
+					
+					instrument = Integer.parseInt(String.valueOf(ins).trim());
+					//System.out.println("\n\n");
+					//System.out.println(instrument);
+					//System.exit(0);
+					synth.setInstrument(instrument);
+					key = 0;
+				}
+				
 			} catch (IOException e1) {e1.printStackTrace();}
+			
 			switch (key) {
-			case '-':
-				counter++;
+			case '-':  /* (1/4)s  */
+				Thread.sleep(250);
 				break;
-			case '.':
-				nano++;
+			case '.':  /* (1/16)s */
+				Thread.sleep(62,500000);
 				break;
-			case 'q':
+			case '_':  /* (1/8)s  */
+				Thread.sleep(125);
+				break;
+			case 'm':  /* Stop All Note */
+				synth.allNotesOff();
+				break;
+			case 'q':  /* EXIT */
 				System.exit(0);
 				break;
-			case 0:
+			case 0:    /* Non Functional keys */
 				break;
-			default:
+			default:   /* Playable Keys */
 				
 				if (key == 'b')
 					try {
 						key = (char) sheet.read();
-						synth.setVolume(110);
+						synth.setVolume(127);
+					} catch (IOException e1) {e1.printStackTrace();}
+				else if (key == 's')
+					try {
+						key = (char) sheet.read();
+						synth.setVolume(90);
 					} catch (IOException e1) {e1.printStackTrace();}
 				else
-					synth.setVolume(80);
+					synth.setVolume(109);
 					
 				try {
 					octave = (char) sheet.read();
@@ -137,12 +235,15 @@ public class MusicSheetReader{
 					note[2] = ' ';
 				}	
 				synth.noteOn(kMap.get(new String(note)));
-				Thread.sleep(250*counter,62500*nano);
-				System.out.println(counter);
 				
 				System.out.println(note);
-				counter = 0;
-				nano = 0;
+				
+				if (flag){
+					flag = false;
+					instrument = 0;
+					synth.setInstrument(instrument);
+				}
+				
 				break;
 			}/*
 			if (key == 'A' || key == 'B' || key == 'C' || key == 'D' || key == 'E' || key == 'F' || key == 'G') {
